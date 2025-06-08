f6b12910080f7a09de0c60e48c2ebd07fac518ba
bobowg <bobowg@163.com>
2025-06-08 22:44:05 +0800

    Revert "添加图片选项"
    This reverts commit 7989a21049b7cfc04aca840da8c3b7854fe7e52b.

--- app/src/main/java/dang/zhou/cun/shoping/MyApp.kt
+++ app/src/main/java/dang/zhou/cun/shoping/MyApp.kt
@@ -1,11 +1,14 @@
 package dang.zhou.cun.shoping
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import dang.zhou.cun.shoping.ui.navigation.InventoryNavHost
 import dang.zhou.cun.shoping.ui.theme.ShoppingTheme
+import dev.chrisbanes.haze.materials.ExperimentalHazeMaterialsApi
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalHazeMaterialsApi::class)
 @Composable
 fun MyApp(navController: NavHostController = rememberNavController()) {
     ShoppingTheme {
